7a461c7f8bc92646893b84c4180d8a777816983a
luke-chant <luke.chant@autotrader.co.uk>
2021-06-08 14:28:15 +0100

    add working r g b buttons

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-function redBackground() {
-        document.body.style.backgroundColor = "red";
+document.getElementById('red').onclick = function redBackground() {
+        document.body.style.backgroundColor = 'red';
+}
+
+document.getElementById('green').onclick = function greenBackground() {
+    document.body.style.backgroundColor = 'green';
+}
+
+document.getElementById('blue').onclick = function blueBackground() {
+    document.body.style.backgroundColor = 'blue';
 }
