03677e725c4a289a2840048842d0776c7643ab72
luke-chant <luke.chant@autotrader.co.uk>
2021-06-08 16:09:14 +0100

    add random colour generator

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// let colour = getElementById(colour);
+
+// document.getElementById(colour).onclick = function background() {
+//     document.body.style.backgroundColor = colour;
+// }
+
 document.getElementById('red').onclick = function redBackground() {
         document.body.style.backgroundColor = 'red';
 }
@@ -9,3 +15,9 @@ document.getElementById('green').onclick = function greenBackground() {
 document.getElementById('blue').onclick = function blueBackground() {
     document.body.style.backgroundColor = 'blue';
 }
+
+
+
+document.getElementById('random').onclick = function randomBackground() {
+    document.body.style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+};
